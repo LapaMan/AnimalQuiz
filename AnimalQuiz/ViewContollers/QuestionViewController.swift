66ca399b9e5889a1655798e06fbeb423d10c20d3
Dzmitry <dmitri.laptenok@gmail.com>
2023-06-19 21:20:42 +0200

    Created an interface and some logic

---
+++ AnimalQuiz/ViewContollers/QuestionViewController.swift
@@ -0,0 +1,110 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by user on 19/06/2023.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+    
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var rangedStackView: UIStackView!
+    
+    @IBOutlet var singleButtons: [UIButton]!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    // MARK: Properties
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var answerChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+}
+
+// MARK: - Private
+extension QuestionViewController {
+    private func updateUI() {
+        // Hide stacks
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        // get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // set current question for label
+        questionLabel.text = currentQuestion.text
+        
+        // calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // set progress for progressView
+        progressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // show current StackView
+        showCurrentStackView(for: currentQuestion.type)
+    }
+    
+    private func showCurrentStackView(for type: ResponseType) {
+        switch type {
+        case .single:
+            showSingleStackView(with: currentAnswers)
+        case .multiple:
+            showMultipleStackView(with: currentAnswers)
+        case .range:
+            showRangedStackView(with: currentAnswers)
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.text, for: .normal)
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden = false
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.text
+        }
+    }
+    
+    private func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden = false
+        
+        rangedLabels.first?.text = answers.first?.text
+        rangedLabels.last?.text = answers.last?.text
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+}
---
+++ AnimalQuiz/ViewContollers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  AnimalQuiz
+//
+//  Created by user on 19/06/2023.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результаты на экране
+    // 4. Избавиться от кнопки возврата на предыдущий экран
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
